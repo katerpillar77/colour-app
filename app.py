3bd6ceb40e90e1b4749787bd20ac717914d91f17
katerpillar77 <47360912+katerpillar77@users.noreply.github.com>
2026-05-24 12:48:45 +0100

    Set row factory for database connection

--- app.py
+++ app.py
@@ -14,7 +14,9 @@ def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
+    db.row_factory = sqlite3.Row
     return db
+
     
 @app.route('/')
 def index():
